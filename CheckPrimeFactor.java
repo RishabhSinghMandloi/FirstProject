64c9799d7b1d2f25eae5de3ed6a657614f8290a0
Rishabh Singh <rishabhsinghmandloi1@gmail.com>
2018-12-12 10:20:02 +0530

    'PrimeFactor'

--- CheckPrimeFactor.java
+++ CheckPrimeFactor.java
@@ -1,4 +1,33 @@
 package org.BridgeLabzs.MyProject;
+import java.util.Scanner;
 public class CheckPrimeFactor{
-   
+   public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Your Number");
+	int num=sc.nextInt();
+	System.out.println("Prime Factor are");
+	primeFactor(num);
+	sc.close();
+}
+   private static void primeFactor(int n)
+   {
+	 for(int i=1;i<=n;i++)
+	 {
+	   if(checkPrime(i)&&n%i==0)
+	   {
+		  System.out.println(i);
+	   }
+	 }
+   }
+   private static boolean checkPrime(int n)
+   {
+ 	  int i=2;        //20
+ 	  while(i<=n/2)
+ 	  {
+ 		  if(n%i==0)
+ 		  return false;
+ 		  i++;
+ 	  }
+ 	 return true;
+   }
 }
